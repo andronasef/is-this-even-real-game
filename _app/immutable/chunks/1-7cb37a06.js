25e131cac435241beb3d4afe931726a9fb73a3b9
andronasef <andronasef@users.noreply.github.com>
2022-12-23 16:11:20 +0000

    deploy: d5098e42231bd637c52473cf91f20d7bf212571d

---
+++ _app/immutable/chunks/1-7cb37a06.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-bb0e9a79.js";export{t as component};
---
+++ _app/immutable/chunks/2-be2d063f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-81c159ca.js";export{t as component};
